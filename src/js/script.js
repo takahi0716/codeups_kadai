2f97d8c45afa1d22a1926002e2b9581113763f1e
oshima <oshima11223344@gmail.com>
2023-09-14 23:17:57 +0900

    9/14

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,12 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   $('.js-hamburger').on('click', function () {
     if ($('.js-hamburger').hasClass('is-open')) {
       // $('.js-drawer-menu').fadeOut();
-      $('.js-drawer-menu').removeClass('is-open');
       $(this).removeClass('is-open');
+      $(".js-sp-nav").fadeOut();
     } else {
       // $('.js-drawer-menu').fadeIn();
       $(this).addClass('is-open');
-      $('.js-drawer-menu').addClass('is-open');
+      $(".js-sp-nav").fadeIn();
     }
   });
 
